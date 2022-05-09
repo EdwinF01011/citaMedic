66eb01bba933b946fc983d5683977327b2fab824
EdwinF01011 <efortega62@misena.edu.co>
2022-05-09 16:36:48 -0500

    Creación del 1er modal

---
+++ index.js
@@ -0,0 +1,27 @@
+// // var btnMedico =document.getElementById('btn btn-medico');
+
+// const openModal=document.querySelector('btn-medico');
+
+// openModall.addEventListener('click',()=>{
+//     alert('click in the button');
+// });
+
+// // var myModal = document.getElementById('myModal')
+// // var myInput = document.getElementById('myInput')
+
+// // myModal.addEventListener('shown.bs.modal', function () {
+// //   myInput.focus()
+// // })
+
+
+
+// //función para eventos del botón
+// function formMedico(){
+//     alert('hola');
+//     btn.style.color="azul";
+// }
+
+// const btn=document.querySelector('.btn-paciente');
+// function cambiarColor(){
+//     btn.style.color="blue";
+// }
